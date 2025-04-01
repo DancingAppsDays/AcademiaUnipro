3f234746043a7656b6cc2361d077210a17a2ea09
Daniel Diaz <dancingappsdays@gmail.com>
2025-04-01 17:55:10 -0600

    fixed home re looks

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.uniprotec.com/api',
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com', // Replace with your actual Google Client ID
-  stripePublishableKey: 'pk_test_your_key',
-};
